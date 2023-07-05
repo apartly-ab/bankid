140010dee38b54db0ead154172a76df0f53b022c
efranz <emilfranzell@gmail.com>
2023-07-05 17:16:42 +0000

    Add LICENSE.md

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,14 @@ import SecretStore from './secretStores/SecretStore';
 import SecretsManagerStore from './secretStores/SecretsManagerStore';
 import SSESTreamStrategy from './strategies/SSEStreamStrategy';
 import BankIdStrategy from './strategies/Strategy';
+import {BankIdClient} from './bankid'
+
+export {
+    CognitoAuthClient,
+    AuthenticationClient,
+    SecretStore,
+    SecretsManagerStore,
+    SSESTreamStrategy,
+    BankIdStrategy,
+    BankIdClient
+}
